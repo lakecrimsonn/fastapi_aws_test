9d392a92d26e003d6a8f75ac34797a623a2eb5d2
lake <lakecrimsonn@gmail.com>
2024-01-29 15:42:32 +0900

    1542

--- api/main.py
+++ api/main.py
@@ -1,10 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from mangum import Mangum
+from dotenv import load_dotenv
+import os
+import shutil
+import requests
+
+load_dotenv()
 
 app = FastAPI()
+
+handler = Mangum(app=app)
 
 @app.get("/")
 async def root():
     return {"message": "hello world"}
 
-handler = Mangum(app=app)
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    # Define the directory where you want to save the image
+    # Make sure this directory exists or create it
+    directory = "images"
+    os.makedirs(directory, exist_ok=True)
+
+    # Define the file path
+    file_path = os.path.join(directory, file.filename)
+
+    # Save the uploaded file to the file path
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    await read_item()
+
+    return {"message": "Image uploaded successfully", "file_path": file_path}
+
+async def read_item():
+    url = os.getenv("AWS_LAMBDA_URL")
+    headers = {"Content-Type": "application/json"}
+
+    data = {
+        "item_id": 123,
+        "name": "Foo",
+        "description": "A very nice Item",
+        "price": 35.4,
+        "tax": 3.2,
+    }
+
+    requests.post(url, json=data, headers=headers)
+    
+    return None
